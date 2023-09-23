c2de5fdad038a9fa09ffe3fae4ecd4d6709b4d4e
matsu-chara <matsuy00@gmail.com>
2023-09-24 08:02:20 +0900

    remove unused header

--- sock.h
+++ sock.h
@@ -1,5 +1,3 @@
-#include <unistd.h>
-
 u_int16_t checksum(u_int8_t *data, int len);
 u_int16_t checksum2(u_int8_t *data1, int len1, u_int8_t *data2, int len2);
 int GetMacAddress(char *device, u_int8_t *hwaddr);
