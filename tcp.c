d2fdabbba819e6922013636af10f5206203f6f06
matsu-chara <matsuy00@gmail.com>
2023-10-29 07:44:23 +0900

    tcp send

--- tcp.c
+++ tcp.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 #include <net/ethernet.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
@@ -292,33 +292,477 @@ int TcpSocketClose(u_int16_t port)
     return 0;
 }
 
+// synを送る。 ackFlag引数によってsyn/ackも送れる
 int TcpSendSyn(int soc, int no, int ackFlag)
 {
     u_int8_t *ptr;
     u_int8_t sbuf[DEFAULT_MTU - sizeof(struct ip)];
     struct tcphdr *tcp;
 
-    
+    ptr = sbuf;
+    tcp = (struct tcphdr *)ptr;
+    memset(tcp, 0, sizeof(struct tcphdr));
+
+    // 以下headerの設定
+    // see https://milestone-of-se.nesuke.com/nw-basic/tcp-udp/tcp-format/
+
+    /* シーケンス番号
+     * シーケンス番号はクライアント側サーバー側で別々の値になり、互いにrandomに初期値を決める。
+     * 値の初期化はクライアントがsyn送信時、サーバーがsyn/ack送信時に行われる。
+     *
+     * それ以降は シーケンス番号 = 初期値 + 相手に送信したデータの byte 数 となる。
+     * ただし、3way ハンドシェイクのパケットについては（データ部が0byteではあるが）例外的に1byteとしてカウントする
+     *
+     * ack_seqはTCP syn送信時は0
+     * それ以降は 応答シーケンス番号 = 前回の相手のシーケンス番号 + 相手から受信したデータの byte 数 となる。
+     * ただし、3way ハンドシェイクのパケットについては（データ部が0byteではあるが）例外的に1byteとしてカウントする
+     */
+    tcp->seq = htonl(TcpTable[no].snd.una);     // シーケンス番号 32bit
+    tcp->ack_seq = htonl(TcpTable[no].rcv.nxt); // 受信シーケンス番号 32bit
+
+    tcp->source = htons(TcpTable[no].myPort); // 16bit = 2byte = 65535まで。送信元がクライアントの場合1024以上のhigher portがよく使われる
+    tcp->dest = htons(TcpTable[no].dstPort);  // 宛先ポート
+    tcp->doff = 5;                            // data offset。TCP ヘッダの長さ (4Bytes単位) が入る。
+
+    // フラグ情報(6個) URG,ACK,PSH,RST,SYN,FIN
+    tcp->urg = 0; // 立ってると緊急を意味する。制御はアプリケーションが決めるためTCPでは何もしない
+    tcp->ack = ackFlag;
+    tcp->psh = 0; // 立ってると、受信側で直ちにアプリケーションにデータを渡せという意味になる。制御はアプリケーションが行うためTCPとしては特に何もしない。(terminalソフトなどで１文字ずつデータを送る場合、バッファせずにレンダリングしてほしい場合にpshフラグを利用するらしい)
+    tcp->rst = 0; // 直ちにtcpを切断する。(synを受信したがポートが利用中の場合や接続シーケンスが不正になった場合に利用する。)
+    tcp->syn = 1;
+    tcp->fin = 0;
+
+    tcp->window = htons(TcpTable[no].snd.wnd); // windowサイズ（ackを待たずに送れるデータサイズ） 受信側が受け入れ可能なデータのバイト数を送信側に知らせるもの。(ackフラグがセットされたときに、このフィールドも埋めるのが良い)
+    tcp->check = htons(0);                     // checksum（後で代入）
+    tcp->urg_ptr = htons(0);                   // urgフラグがONの場合に緊急データの位置を示す(実際にはsequence番号 + 緊急データのバイト数での位置が入る)
+
+    ptr += sizeof(struct tcphdr);
+
+    tcp->check = TcpChecksum(&Param.vip, &TcpTable[no].dstAddr, IPPROTO_TCP, (u_int8_t *)sbuf, ptr - sbuf);
+
+    printf("=== TCP ===[\n");
+    IpSend(soc, &Param.vip, &TcpTable[no].dstAddr, IPPROTO_TCP, 1, Param.IpTTL, sbuf, ptr - sbuf);
+    print_tcp(tcp);
+    printf("]\n");
+
+    TcpTable[no].snd.nxt = TcpTable[no].snd.una;
+
+    return 0;
 }
 
-int TcpSendFin(int soc, int no);
+int TcpSendFin(int soc, int no)
+{
+    u_int8_t *ptr;
+    u_int8_t sbuf[DEFAULT_MTU - sizeof(struct ip)];
+    struct tcphdr *tcp;
 
-int TcpSendRst(int soc, int no);
+    ptr = sbuf;
+    tcp = (struct tcphdr *)ptr;
+    memset(tcp, 0, sizeof(struct tcphdr));
 
-int TcpSendAck(int soc, int no);
+    tcp->seq = htonl(TcpTable[no].snd.una);
+    tcp->ack_seq = htonl(TcpTable[no].rcv.nxt);
 
-int TcpSendRstDirect(int soc, struct ether_header *r_eh, struct ip *r_ip, struct tcphdr *r_tcp);
+    tcp->source = htons(TcpTable[no].myPort);
+    tcp->dest = htons(TcpTable[no].dstPort);
+    tcp->doff = 5;
 
-int TcpConnect(int soc, u_int16_t sport, struct in_addr *daddr, u_int16_t dport);
+    tcp->urg = 0;
+    tcp->ack = 1;
+    tcp->psh = 0;
+    tcp->rst = 0;
+    tcp->syn = 0;
+    tcp->fin = 1;
 
-int TcpClose(int soc, u_int16_t sport);
+    tcp->window = htons(TcpTable[no].snd.wnd);
+    tcp->check = htons(0);
+    tcp->urg_ptr = htons(0);
 
-int TcpReset(int soc, u_int16_t sport);
+    ptr += sizeof(struct tcphdr);
 
-int TcpAllSocketClose(int soc);
+    tcp->check = TcpChecksum(&Param.vip, &TcpTable[no].dstAddr, IPPROTO_TCP, (u_int8_t *)sbuf, ptr - sbuf);
 
-int TcpSendData(int soc, u_int16_t sport, u_int8_t *data, int len);
+    printf("=== TCP ===[\n");
+    IpSend(soc, &Param.vip, &TcpTable[no].dstAddr, IPPROTO_TCP, 1, Param.IpTTL, sbuf, ptr - sbuf);
+    print_tcp(tcp);
+    printf("]\n");
 
-int TcpSend(int soc, u_int16_t sport, u_int8_t *data, int len);
+    TcpTable[no].snd.nxt = TcpTable[no].snd.una;
+
+    return 0;
+}
+
+int TcpSendRst(int soc, int no)
+{
+    u_int8_t *ptr;
+    u_int8_t sbuf[DEFAULT_MTU - sizeof(struct ip)];
+    struct tcphdr *tcp;
+
+    ptr = sbuf;
+    tcp = (struct tcphdr *)ptr;
+    memset(tcp, 0, sizeof(struct tcphdr));
+
+    tcp->seq = htonl(TcpTable[no].snd.una);
+    tcp->ack_seq = htonl(TcpTable[no].rcv.nxt);
+
+    tcp->source = htons(TcpTable[no].myPort);
+    tcp->dest = htons(TcpTable[no].dstPort);
+    tcp->doff = 5;
+
+    tcp->urg = 0;
+    tcp->ack = 1;
+    tcp->psh = 0;
+    tcp->rst = 1;
+    tcp->syn = 0;
+    tcp->fin = 0;
+
+    tcp->window = htons(TcpTable[no].snd.wnd);
+    tcp->check = htons(0);
+    tcp->urg_ptr = htons(0);
+
+    ptr += sizeof(struct tcphdr);
+
+    tcp->check = TcpChecksum(&Param.vip, &TcpTable[no].dstAddr, IPPROTO_TCP, (u_int8_t *)sbuf, ptr - sbuf);
+
+    printf("=== TCP ===[\n");
+    IpSend(soc, &Param.vip, &TcpTable[no].dstAddr, IPPROTO_TCP, 1, Param.IpTTL, sbuf, ptr - sbuf);
+    print_tcp(tcp);
+    printf("]\n");
+
+    TcpTable[no].snd.nxt = TcpTable[no].snd.una;
+
+    return 0;
+}
+
+int TcpSendAck(int soc, int no)
+{
+    // ack応答は毎回やるのではなく最後に受信したseq_numに対して行う。送信側はそれまでに送信したすべてのセグメントに対してackが来たものとみなす。
+    // 欠損がある場合は、欠損直前までのseq_numについてackを返して、それ以降のデータをすべて再送してもらう。
+    // ただしTCP optionでSACK (selective ack)に対応している場合は受信したseq_numを(先頭~欠損の末尾, 欠損の次~次の欠損の末尾, ...)のように列挙してackすることができる。（対応しているかどうかは3way handshake時に対応オプションを通知しあう）
+
+    u_int8_t *ptr;
+    u_int8_t sbuf[sizeof(struct ether_header) + DEFAULT_MTU];
+    struct tcphdr *tcp;
+
+    ptr = sbuf;
+    tcp = (struct tcphdr *)ptr;
+    memset(tcp, 0, sizeof(struct tcphdr));
+
+    tcp->seq = htonl(TcpTable[no].snd.una);
+    tcp->ack_seq = htonl(TcpTable[no].rcv.nxt);
+
+    tcp->source = htons(TcpTable[no].myPort);
+    tcp->dest = htons(TcpTable[no].dstPort);
+    tcp->doff = 5;
+
+    tcp->urg = 0;
+    tcp->ack = 1;
+    tcp->psh = 0;
+    tcp->rst = 0;
+    tcp->syn = 0;
+    tcp->fin = 0;
+
+    tcp->window = htons(TcpTable[no].snd.wnd);
+    tcp->check = htons(0);
+    tcp->urg_ptr = htons(0);
+
+    ptr += sizeof(struct tcphdr);
+
+    tcp->check = TcpChecksum(&Param.vip, &TcpTable[no].dstAddr, IPPROTO_TCP, (u_int8_t *)sbuf, ptr - sbuf);
+
+    printf("=== TCP ===[\n");
+    IpSend(soc, &Param.vip, &TcpTable[no].dstAddr, IPPROTO_TCP, 1, Param.IpTTL, sbuf, ptr - sbuf);
+    print_tcp(tcp);
+    printf("]\n");
+
+    TcpTable[no].snd.nxt = TcpTable[no].snd.una;
+
+    return 0;
+}
+
+// 受信対象ではないポートからTCPパケットが届いた場合に送るRSTパケット
+int TcpSendRstDirect(int soc, struct ether_header *r_eh, struct ip *r_ip, struct tcphdr *r_tcp)
+{
+    u_int8_t *ptr;
+    u_int8_t sbuf[sizeof(struct ether_header) + DEFAULT_MTU];
+    struct tcphdr *tcp;
+
+    ptr = sbuf;
+    tcp = (struct tcphdr *)ptr;
+    memset(tcp, 0, sizeof(struct tcphdr));
+
+    tcp->seq = r_tcp->ack_seq;
+    tcp->ack_seq = htonl((r_tcp->seq) + 1);
+
+    tcp->source = r_tcp->dest;
+    tcp->dest = r_tcp->source;
+    tcp->doff = 5;
+
+    tcp->urg = 0;
+    tcp->ack = 1;
+    tcp->psh = 0;
+    tcp->rst = 1;
+    tcp->syn = 0;
+    tcp->fin = 0;
+
+    tcp->window = 0;
+    tcp->check = htons(0);
+    tcp->urg_ptr = htons(0);
+
+    ptr += sizeof(struct tcphdr);
+
+    tcp->check = TcpChecksum(&Param.vip, &r_ip->ip_src, IPPROTO_TCP, (u_int8_t *)sbuf, ptr - sbuf);
+
+    printf("=== TCP ===[\n");
+    IpSend(soc, &Param.vip, &r_ip->ip_src, IPPROTO_TCP, 1, Param.IpTTL, sbuf, ptr - sbuf);
+    print_tcp(tcp);
+    printf("]\n");
+
+    return 0;
+}
+
+int TcpConnect(int soc, u_int16_t sport, struct in_addr *daddr, u_int16_t dport)
+{
+    int count, no;
+
+    if ((no = TcpAddTable(sport)) == -1)
+    {
+        return -1;
+    }
+
+    TcpTable[no].dstPort = dport;
+    TcpTable[no].dstAddr.s_addr = daddr->s_addr;
+    TcpTable[no].status = TCP_SYN_SENT;
+    count = 0;
+    do
+    {
+        TcpSendSyn(soc, no, 0);
+        DummyWait(DUMMY_WAIT_MS * (count + 1));
+        printf("TcpConnect:%s\n", TcpStatusStr(TcpTable[no].status));
+        count++;
+        if (count > RETRY_COUNT)
+        {
+            printf("TcpConnect:retry over\n");
+            TcpSocketClose(sport);
+            return 0;
+        }
+    } while (TcpTable[no].status != TCP_ESTABLISHED);
+
+    printf("TcpConnect:success\n");
+
+    return 1;
+}
+
+int TcpClose(int soc, u_int16_t sport)
+{
+    int count, no;
+    time_t now_t;
+
+    if ((no = TcpAddTable(sport)) == -1)
+    {
+        return -1;
+    }
+
+    if (TcpTable[no].status == TCP_ESTABLISHED)
+    {
+        TcpTable[no].status = TCP_FIN_WAIT1;
+        count = 0;
+        do
+        {
+            TcpSendFin(soc, no); // FINを送る
+            DummyWait(DUMMY_WAIT_MS * (count + 1));
+            printf("TcpClose:%s\n", TcpStatusStr(TcpTable[no].status));
+            count++;
+            if (count > RETRY_COUNT)
+            {
+                printf("TcpClose:retry over\n");
+                TcpSocketClose(sport);
+                return 0;
+            }
+
+            // 次のどちらかになるのを待つ
+            // - TCP_FIN_WAIT2 (fin/ack受信, fin受信待ち)
+            // - TCP_CLOSING (fin受信, fin/ack受信待ち)
+            // （いきなり RST パケットを送られて TCP_CLOSE になることもある）
+        } while (TcpTable[no].status == TCP_FIN_WAIT1);
+    }
+
+    count = 0;
+
+    // 次のどちらかかを待つ
+    // - TCP_FIN_WAIT2 =>  fin受信待ち
+    // - TCP_CLOSING => fin/ack受信待ち
+    // どちらもメッセージ受信時には TCP_TIME_WAIT に移行する。
+    // いきなり RST パケットを送られて TCP_CLOSE になることもあるので TCP_CLOSE でないことも確認する
+    while (TcpTable[no].status != TCP_TIME_WAIT && TcpTable[no].status != TCP_CLOSE)
+    {
+        DummyWait(DUMMY_WAIT_MS * (count + 1));
+        printf("TcpClose:%s\n", TcpStatusStr(TcpTable[no].status));
+        count++;
+        if (count > RETRY_COUNT)
+        {
+            printf("TcpClose:retry over\n");
+            TcpSocketClose(sport);
+            return 0;
+        }
+    }
+
+    // TCP_TIME_WAIT から TCP_CLOSE に移行するのを一定時間待つ
+    // いきなり RST パケットを送られて TCP_CLOSE になることもあるので TCP_CLOSE でないことを確認する
+    if (TcpTable[no].status != TCP_CLOSE)
+    {
+        now_t = time(NULL);
+        while (time(NULL) - now_t < TCP_FIN_TIMEOUT)
+        {
+            printf("TcpClose:status=%s", TcpStatusStr(TcpTable[no].status));
+            sleep(1);
+        }
+        // 一定時間経過したのでcloseする。
+        TcpTable[no].status = TCP_CLOSE;
+    }
+
+    printf("TcpClose:status=%s:success\n", TcpStatusStr(TcpTable[no].status));
+    if (TcpTable[no].myPort != 0)
+    {
+        TcpSocketClose(sport);
+    }
+
+    return 1;
+}
+
+int TcpReset(int soc, u_int16_t sport)
+{
+    int no;
+
+    if ((no = TcpSearchTable) == -1)
+    {
+        return -1;
+    }
+
+    TcpSendRst(soc, no);
+    TcpSocketClose(sport);
+
+    return 1;
+}
+
+int TcpAllSocketClose(int soc)
+{
+    for (int i = 0; i < TCP_TABLE_NO; i++)
+    {
+        if (TcpTable[i].myPort != 0 && TcpTable[i].status == TCP_ESTABLISHED)
+        {
+            TcpClose(soc, TcpTable[i].myPort);
+        }
+    }
+
+    return 0;
+}
+
+int TcpSendData(int soc, u_int16_t sport, u_int8_t *data, int len)
+{
+    u_int8_t *ptr;
+    u_int8_t sbuf[DEFAULT_MTU - sizeof(struct ip)];
+    int no;
+    struct tcphdr *tcp;
+
+    if ((no = TcpSearchTable(sport)) == -1)
+    {
+        return -1;
+    }
+
+    if (TcpTable[no].status != TCP_ESTABLISHED)
+    {
+        printf("TcpSend:not established\n");
+        return -1;
+    }
+
+    ptr = sbuf;
+    tcp = (struct tcphdr *)ptr;
+    memset(tcp, 0, sizeof(struct tcphdr));
+    tcp->seq = htonl(TcpTable[no].snd.una);
+    tcp->ack_seq = htonl(TcpTable[no].snd.una);
+    tcp->source = htons(TcpTable[no].myPort);
+    tcp->dest = htons(TcpTable[no].dstPort);
+    tcp->doff = 5;
+    tcp->urg = 0;
+    tcp->ack = 1;
+    tcp->psh = 0;
+    tcp->rst = 0;
+    tcp->syn = 0;
+    tcp->fin = 0;
+    tcp->window = htons(TcpTable[no].snd.wnd);
+    tcp->check = htons(0);
+    tcp->urg_ptr = htons(0);
+
+    ptr += sizeof(struct tcphdr);
+
+    memcpy(ptr, data, len);
+    ptr += len;
+
+    tcp->check = TcpChecksum(&Param.vip, &TcpTable[no].dstAddr, IPPROTO_TCP, (u_int8_t *)sbuf, ptr - sbuf);
+
+    printf("=== TCP ===[\n");
+    IpSend(soc, &Param.vip, &TcpTable[no].dstAddr, IPPROTO_TCP, 0, Param.IpTTL, sbuf, ptr - sbuf);
+    print_tcp(tcp);
+    print_hex(data, len);
+    printf("]\n");
+
+    TcpTable[no].snd.nxt = TcpTable[no].snd.una + len;
+
+    return 0;
+}
+
+int TcpSend(int soc, u_int16_t sport, u_int8_t *data, int len)
+{
+    u_int8_t *ptr;
+    int count, no;
+    int lest, sndLen;
+    if ((no = TcpSearchTable(sport) == -1))
+    {
+        return -1;
+    }
+
+    ptr = data;
+    lest = len;
+
+    while (lest > 0)
+    {
+        if (lest >= TcpTable[no].rcv.wnd)
+        {
+            sndLen = TcpTable[no].rcv.wnd;
+        }
+        else if (lest >= Param.MSS)
+        {
+            sndLen = Param.MSS;
+        }
+        else
+        {
+            sndLen = lest;
+        }
+
+        printf("TcpSend:offset=%ld,len=%d,lest=%d\n", ptr - data, sndLen, lest);
+
+        count = 0;
+        do
+        {
+            TcpSendData(soc, sport, ptr, sndLen);
+            DummyWait(DUMMY_WAIT_MS * (count + 1));
+            printf("TcpSend:una=%u,nextSeq=%u\n", TcpTable[no].snd.una, -TcpTable[no].snd.iss, TcpTable[no].snd.nxt - TcpTable[no].snd.iss);
+            count++;
+            if (count > RETRY_COUNT)
+            {
+                printf("TcpSend:retry over\n");
+                return 0;
+            }
+        } while (TcpTable[no].snd.una != TcpTable[no].snd.nxt);
+
+        ptr += sndLen;
+        lest -= sndLen;
+    }
+
+    printf("TcpSend:una=%u,nextSeq=%u:success\n", TcpTable[no].snd.una - TcpTable[no].snd.iss, TcpTable[no].snd.nxt - TcpTable[no].snd.iss);
+
+    return 1;
+}
 
 int TcpRecv(int soc, struct ether_header *eh, struct ip *ip, u_int8_t *data, int len);
